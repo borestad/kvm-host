650c315035c6c010cd6d3dd0393a452d530dbc2f
Johan Borestad <johan@borestad.com>
2015-05-10 23:58:22 +0200

    Update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -e
 
+cd /tmp
 apt-get -y install git
-rm -rf /tmp/kvm-host && cd /tmp && git clone https://github.com/borestad/kvm-host.git && ./install.sh
+rm -rf /tmp/kvm-host
+
+git clone https://github.com/borestad/kvm-host.git
+cd kvm-host
+./install.sh
 
